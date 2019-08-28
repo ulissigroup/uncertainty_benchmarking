b122bed16b3602876395750fb3cb651825718d13
Kevin Tran <ktrain9891@gmail.com>
2019-08-28 15:43:06 -0400

    Initial benchmark for ensembled CGCNN

---
+++ CGCNN_ensemble/fit_ensembles.py
@@ -0,0 +1,76 @@
+import pickle
+import numpy as np
+from sklearn.model_selection import KFold
+import torch
+from torch.optim import Adam
+import skorch.callbacks.base
+from skorch.callbacks import Checkpoint  # needs skorch >= 0.4
+from skorch.callbacks.lr_scheduler import LRScheduler
+from skorch import NeuralNetRegressor
+from cgcnn.model import CrystalGraphConvNet
+from cgcnn.data import collate_pool, MergeDataset
+
+
+# Find and use the appropriate GPU/CPU
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+    print('Using GPU')
+else:
+    device = 'cpu'
+    print('Using CPU')
+
+
+# Load the data split from our Jupyter notebook cache
+with open('../preprocessing/feature_dimensions.pkl', 'rb') as file_handle:
+    orig_atom_fea_len, nbr_fea_len = pickle.load(file_handle)
+with open('../preprocessing/splits.pkl', 'rb') as file_handle:
+    splits = pickle.load(file_handle)
+sdts_train = splits['sdts_train']
+targets_train = splits['targets_train']
+
+
+class train_end_load_best_valid_loss(skorch.callbacks.base.Callback):
+    def on_train_end(self, net, X, y):
+        net.load_params('./histories/%i_valid_best_params.pt' % k)
+
+
+# Fold the CV data
+k_folder = KFold(n_splits=5)
+for k, (indices_train, _) in enumerate(k_folder.split(sdts_train)):
+    stds_train_ = [sdts_train[index] for index in indices_train]
+    targets_train_ = np.array([targets_train[index] for index in indices_train])
+
+    # Define various callbacks and checkpointers for this network
+    LR_schedule = LRScheduler('MultiStepLR', milestones=[75], gamma=0.1)
+    cp = Checkpoint(monitor='valid_loss_best', fn_prefix='./histories/%i_valid_best_' % k)
+    load_best_valid_loss = train_end_load_best_valid_loss()
+
+    # Train this fold's network
+    net = NeuralNetRegressor(
+        CrystalGraphConvNet,
+        module__orig_atom_fea_len=orig_atom_fea_len,
+        module__nbr_fea_len=nbr_fea_len,
+        batch_size=214,
+        module__classification=False,
+        lr=0.0056,
+        max_epochs=100,
+        module__atom_fea_len=46,
+        module__h_fea_len=83,
+        module__n_conv=8,
+        module__n_h=4,
+        optimizer=Adam,
+        iterator_train__pin_memory=True,
+        iterator_train__num_workers=0,
+        iterator_train__collate_fn=collate_pool,
+        iterator_train__shuffle=True,
+        iterator_valid__pin_memory=True,
+        iterator_valid__num_workers=0,
+        iterator_valid__collate_fn=collate_pool,
+        iterator_valid__shuffle=False,
+        device=device,
+        criterion=torch.nn.L1Loss,
+        dataset=MergeDataset,
+        callbacks=[cp, load_best_valid_loss, LR_schedule]
+    )
+    net.initialize()
+    net.fit(stds_train_, targets_train_)
---
+++ CGCNN_ensemble/submit_ensemble_fitting.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#SBATCH --nodes=1
+#SBATCH --constraint=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --account=m1759
+#SBATCH --job-name=cgcnn_ensemble
+#SBATCH --out=ensembling.out
+#SBATCH --error=ensembling.error
+#SBATCH --mail-user=ktran@andrew.cmu.edu
+#SBATCH --mail-type=ALL
+#SBATCH --time=04:00:00
+
+
+source ~/miniconda3/bin/activate
+conda activate gaspy
+python fit_ensembles.py
