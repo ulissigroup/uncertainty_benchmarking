1c3275fc2455a273256268e6488307f2450f8bb0
Kevin Tran <ktrain9891@gmail.com>
2019-09-11 10:39:13 -0400

    Try pulling other adsorbate data from catalysis-hub

--- preprocessing/pull_data/catalysis-hub/pull.py
+++ preprocessing/pull_data/catalysis-hub/pull.py
@@ -1,8 +1,8 @@
 '''
 This script will pull data from catalysis hub and then save it as an ase
-database. It turns out that catalysis hub has 53 hydrogen adsorption energies
-and 446 CO adsorption energies. That's not enough for us, so we don't bother
-saving the ase database.
+database. It turns out that catalysis hub has O(10--100) adsorption energies
+per adsorbate. That's not enough for us, so we don't bother saving the ase
+database.
 '''
 
 import os
@@ -14,60 +14,56 @@ import ase.db
 from ase.io import read
 
 
-query_string =\
-"""
-{reactions(first: 50, products: "Hstar", reactants: "star+Hgas") {
- totalCount
-  edges {
-    node {
-      chemicalComposition
-      surfaceComposition
-      facet
-      sites
-      coverages
-      reactants
-      products
-      Equation
-      reactionEnergy
-      activationEnergy
-      dftCode
-      dftFunctional
-      username
-      pubId
-      systems {
-      	id
-        Trajdata
-        keyValuePairs
+for ads in ['C', 'O', 'N', 'H', 'S', 'OH', 'NH', 'SH', 'CO']:
+
+    # Make the query for catalysis-hub
+    query_string =\
+    """
+    {reactions(first: 50, products: "%sstar", reactants: "star+%sgas") {
+     totalCount
+      edges {
+        node {
+          facet
+          sites
+          coverages
+          reactionEnergy
+          dftCode
+          dftFunctional
+          username
+          pubId
+          systems {
+            Trajdata
+            keyValuePairs
+          }
+        }
       }
-    }
-  }
-}}
-"""
+    }}
+    """ % (ads, ads)
 
-root = 'http://api.catalysis-hub.org/graphql'
-data = requests.post(root, {'query': query_string})
+    # Grab the data
+    root = 'http://api.catalysis-hub.org/graphql'
+    data = requests.post(root, {'query': query_string}).json()['data']
+    print('%i total structures for %s ' % (data['reactions']['totalCount'], ads))
 
-try:
-    data = data.json()['data']
-except:
-    print('Error')
+    # Delete the current `reactions.db` file so we don't add redundant data
+    try:
+        os.remove('reactions.db')
+    except FileNotFoundError:
+        pass
+    db = ase.db.connect('reactions.db')
 
-print('%i total structures' % data['reactions']['totalCount'])
-try:
-    os.remove('reactions.db')
-except FileNotFoundError:
-    pass
+    # Get the energies
+    for reaction in tqdm(data['reactions']['edges'], desc='Reactions'):
+        reaction_data = reaction['node']
+        reaction_energy = reaction_data['reactionEnergy']
 
-db = ase.db.connect('reactions.db')
-for reaction in tqdm(data['reactions']['edges'], desc='Reactions'):
-    reaction_data = reaction['node']
-    reaction_energy = reaction_data['reactionEnergy']
+        # Get the structures
+        for structure in tqdm(reaction_data['systems'], desc='Structures'):
+            atoms = read(io.StringIO(structure['Trajdata']), format='json')
+            key_value_pairs = json.loads(structure['keyValuePairs'])
 
-    for structure in tqdm(reaction_data['systems'], desc='Structures'):
-        atoms = read(io.StringIO(structure['Trajdata']), format='json')
-        key_value_pairs = json.loads(structure['keyValuePairs'])
-
-        if len(atoms) > 12:
-            key_value_pairs.update(adsorption_energy=reaction_energy)
-            db.write(atoms, key_value_pairs=key_value_pairs)
-print()
+            # Filter out adsorbates and other small systems
+            if len(atoms) > 12:
+                key_value_pairs.update(adsorption_energy=reaction_energy)
+                db.write(atoms, key_value_pairs=key_value_pairs)
+    print()
