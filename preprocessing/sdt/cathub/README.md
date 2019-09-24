55ca5236c3cd0acecd705f06c69c6d7476338c84
Kevin Tran <ktrain9891@gmail.com>
2019-09-24 13:51:22 -0400

    Reorganize SDT'ing folder and SDT CatHub data
    SDT = `StructureDataTransformer`, which is the preprocessing step that
    we perform on atomic structures so that we can feed them to CGCNN

---
+++ preprocessing/sdt/cathub/README.md
@@ -0,0 +1,2 @@
+The `chunk_data.py` file creates a number of `cathub_*.pkl` files, which `create_sdt.py` needs to run.
+The `cathub_*.pkl` files are ignored by this repository to save space.
---
+++ preprocessing/sdt/cathub/chunk_data.py
@@ -0,0 +1,22 @@
+'''
+The StructureDataTransformer apparently takes a lot of memory. We address this
+by dividing our datasets into smaller chunks, and then letting the transformer
+handle one chunk at a time. This script divides the data into chunks.
+'''
+
+import pickle
+from gaspy.utils import _chunk
+
+
+# Load the documents
+with open('preprocessed_cathub.pkl', 'rb') as file_handle:
+    docs = pickle.load(file_handle)
+
+# Create an iterator that yields the chunks
+chunk_size = 5000
+docs_iterator = _chunk(docs, chunk_size)
+
+# Save the chunks
+for i, _docs in enumerate(docs_iterator):
+    with open('cathub_%i.pkl' % i, 'wb') as file_handle:
+        pickle.dump(_docs, file_handle)
